e0a781b3aa4db1b3c2ecadf1bb1e731399ecf5c9
rewerma <rewerma@163.com>
2016-04-24 23:43:51 +0800

    fix bug

--- pla-core/src/main/java/com/pla/model/Model.java
+++ pla-core/src/main/java/com/pla/model/Model.java
@@ -1,34 +1,27 @@
 package com.pla.model;
 
+@SuppressWarnings("unchecked")
 public abstract class Model<T> extends ModelFinder<T> {
-    /**
-     * It will be called before save data.
-     */
     protected void init() {
     }
 
-    @SuppressWarnings("unchecked")
     public void save() {
         init();
         getDao().save((T) this);
     }
 
-    @SuppressWarnings("unchecked")
     public void update() {
         getDao().update((T) this);
     }
 
-    @SuppressWarnings("unchecked")
     public void update(String... fields) {
         getDao().update((T) this, fields);
     }
 
-    @SuppressWarnings("unchecked")
     public void saveOrUpdate() {
         getDao().saveOrUpdate((T) this);
     }
 
-    @SuppressWarnings("unchecked")
     public void delete() {
         getDao().delete((T) this);
     }
